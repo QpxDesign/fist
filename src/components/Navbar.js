5d688120eeeb3175d0836ff57ec1186d161ef3e3
qpxdesign <qroshan5@gmail.com>
2022-05-03 19:36:05 -0400

    added dropdown menu to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,17 @@ export default function Navbar({ logo }) {
       <img src={logo} alt="Logo" className="logo" />
       <div className="navbar-options-wrapper">
         <div className="navbar-option">
-          <h2>Home</h2>
+          <h2>Learn More</h2>
         </div>
         <div className="navbar-option">
           <h2>Contact</h2>
         </div>
         <div className="join-button">
           <h2>Get Started</h2>
+        </div>
+        <div className="dropdown-toggle">≡</div>
+        <div className="dropdown-wrapper">
+          <div className="dropdown-option"></div>
         </div>
       </div>
     </div>
