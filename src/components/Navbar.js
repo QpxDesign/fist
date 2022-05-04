f825a72ab3fda83475d6a5c22a39c9cdb220b835
qpxdesign <qroshan5@gmail.com>
2022-05-03 20:16:35 -0400

    finished responsive navbar component\

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function Navbar({ logo }) {
+  const dropdownRef = useRef(null);
+
   return (
-    <div className="navbar-wrapper">
-      <img src={logo} alt="Logo" className="logo" />
-      <div className="navbar-options-wrapper">
-        <div className="navbar-option">
-          <h2>Learn More</h2>
-        </div>
-        <div className="navbar-option">
-          <h2>Contact</h2>
-        </div>
-        <div className="join-button">
-          <h2>Get Started</h2>
-        </div>
-        <div className="dropdown-toggle">≡</div>
-        <div className="dropdown-wrapper">
-          <div className="dropdown-option"></div>
+    <>
+      <div className="navbar-wrapper">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="navbar-options-wrapper">
+          <div className="navbar-option">
+            <h2>Learn More</h2>
+          </div>
+          <div className="navbar-option">
+            <h2>Contact</h2>
+          </div>
+          <div className="join-button">
+            <h2>Get Started</h2>
+          </div>
+          <div
+            className="dropdown-toggle"
+            onClick={() => {
+              if (dropdownRef.current.style.display != "none") {
+                dropdownRef.current.style.display = "none";
+              } else {
+                dropdownRef.current.style.display = "flex";
+              }
+            }}
+          >
+            ≡
+          </div>
         </div>
       </div>
-    </div>
+      <div className="dropdown-wrapper" ref={dropdownRef}>
+        <div className="dropdown-option">Learn More</div>
+        <div className="dropdown-option">Contact</div>
+        <div className="dropdown-option">Get Started</div>
+      </div>
+    </>
   );
 }
