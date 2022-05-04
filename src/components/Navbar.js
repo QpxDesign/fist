a73427681c80e7d761a588f6c8406132c98a409e
qpxdesign <qroshan5@gmail.com>
2022-05-04 15:10:23 -0400

    allowed user to select active filter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { FaSearch, FaLocationArrow } from "react-icons/fa";
 import { GoIssueOpened } from "react-icons/go";
 import { RiPinDistanceFill } from "react-icons/ri";
@@ -8,6 +8,21 @@ import {
   BsThreeDots,
 } from "react-icons/bs";
 export default function Navbar({ logo }) {
+  const [ActiveFilterIndex, setActiveFilterIndex] = useState(-1);
+  function toggleActiveFilterIndex(index) {
+    if (ActiveFilterIndex === index) {
+      setActiveFilterIndex(-1);
+    } else setActiveFilterIndex(index);
+  }
+  function setActiveFilter(index) {
+    // remove active class from all elements
+    // set active element to be "active" class on click
+    var newClassName = "parameters-option ";
+    if (index === ActiveFilterIndex) {
+      return newClassName + "active";
+    }
+    return newClassName;
+  }
   return (
     <>
       <img src={logo} alt="logo" className="logo" />
@@ -22,27 +37,45 @@ export default function Navbar({ logo }) {
           </button>
         </div>
         <div className="parameters-wrapper">
-          <div className="parameters-option">
+          <div
+            className={setActiveFilter(0)}
+            onClick={() => toggleActiveFilterIndex(0)}
+          >
             <GoIssueOpened className="parameter-icon" />
             Issues
           </div>
-          <div className="parameters-option">
+          <div
+            className={setActiveFilter(1)}
+            onClick={() => toggleActiveFilterIndex(1)}
+          >
             <BsFillPeopleFill className="parameter-icon" />
             Size
           </div>
-          <div className="parameters-option">
+          <div
+            className={setActiveFilter(2)}
+            onClick={() => toggleActiveFilterIndex(2)}
+          >
             <RiPinDistanceFill className="parameter-icon" />
             Distance
           </div>
-          <div className="parameters-option">
+          <div
+            className={setActiveFilter(3)}
+            onClick={() => toggleActiveFilterIndex(3)}
+          >
             <BsCalendarMinusFill className="parameter-icon" />
             Date/Time
           </div>
-          <div className="parameters-option">
+          <div
+            className={setActiveFilter(4)}
+            onClick={() => toggleActiveFilterIndex(4)}
+          >
             <BsThreeDots className="parameter-icon" />
             More
           </div>
-          <div className="showfiltermenu-button parameters-option">
+          <div
+            className={setActiveFilter(5)}
+            onClick={() => toggleActiveFilterIndex(5)}
+          >
             <BsThreeDots className="parameter-icon" />
             Show Filters
           </div>
